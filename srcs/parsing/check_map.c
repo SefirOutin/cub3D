af2c40372661ab982df4d6a8c66560c723584495
SefirOutin <1stalias.stove861@passinbox.com>
2024-02-25 21:38:12 +0100

    filling color param and cheking map config

--- srcs/parsing/check_map.c
+++ srcs/parsing/check_map.c
@@ -6,9 +6,72 @@
 /*   By: soutin <soutin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 16:39:40 by soutin            #+#    #+#             */
-/*   Updated: 2024/02/25 16:39:51 by soutin           ###   ########.fr       */
+/*   Updated: 2024/02/25 21:29:27 by soutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+int tab_size(char **tab)
+{
+    int i;
+    
+    i = 0;
+    while (tab[i])
+        i++;
+    return (i);
+}
+
+int prout(char **map, int x, int y)
+{
+	int	player;
+	
+	player = 0;
+    if (!x || x == ft_strlen(map[y]) - 1)
+    {
+		if (ft_strchr("0NSWE", map[y][x]))
+        	return (1);
+	}
+    else if (map[y][x] == '0'
+			&& ((map[y][x + 1] == ' ' || map[y][x - 1] == ' ')
+			|| (x > ft_strlen(map[y - 1]) - 1 || x > ft_strlen(map[y + 1]) - 1)
+			|| (x <= ft_strlen(map[y - 1]) - 1 && map[y - 1][x] == ' ')
+			|| (x <= ft_strlen(map[y + 1]) - 1 && map[y + 1][x] == ' ')))
+			return (1);
+	// else if (map[y][x] == '0' && (map[y][x + 1] == ' ' || map[y][x - 1] == ' '))
+	//     return (1);
+    // else if (map[y][x] == '0' && (x > ft_strlen(map[y - 1]) - 1 || x > ft_strlen(map[y + 1]) - 1))
+	// 	return (1);
+	// else if (map[y][x] == '0' && (x <= ft_strlen(map[y - 1]) - 1 && map[y - 1][x] == ' '))
+	// 	return (1);
+	// else if (map[y][x] == '0' && (x <= ft_strlen(map[y + 1]) - 1 && map[y + 1][x] == ' '))
+	// 	return (1);
+	return (0);
+}
+
+int check_map(t_vars *vars)
+{
+    int x;
+    int y;
+
+    y = 0;
+    while (vars->map[y])
+    {
+        x = 0;
+        while (vars->map[y][x])
+        {
+            if (!ft_strchr("01NSEW\n ", vars->map[y][x]))
+                return (printf("error: invalid character\n"), 1);
+            if (!y || y == tab_size(vars->map) - 1)
+            {
+                if (!ft_strchr(" 1\n", vars->map[y][x]))
+                    return (printf("error: unclosed map\n"), 1);
+            }
+            else if (prout(vars->map, x, y))
+                return (printf("error: unclosed map\n"), 1);
+            x++;
+        }
+        y++;
+    }
+    return (0);
+}
--- srcs/parsing/get_map_data.c
+++ srcs/parsing/get_map_data.c
@@ -6,7 +6,7 @@
 /*   By: soutin <soutin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 15:52:16 by soutin            #+#    #+#             */
-/*   Updated: 2024/02/25 16:03:33 by soutin           ###   ########.fr       */
+/*   Updated: 2024/02/25 21:22:20 by soutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,8 @@ int	get_textures(t_vars *vars, int fd, int *error)
 		free(tmp);
 		skip++;
 	}
+	if (nb_textures != 6)
+		return (-1);
 	return (skip);
 }
 
@@ -71,16 +73,18 @@ int	fill_textures_vars2(t_vars *vars, char *tmp, int *nb_textures)
 	}
 	else if (!ft_strncmp("F ", tmp, 2) && ++(*nb_textures))
 	{
-		vars->floor_color = ft_substr(tmp, 2, ft_strlen(tmp + 2));
-		if (!vars->floor_color)
-			return (-1);
+		if (fill_colors(vars->floor_color, tmp + 1))
+			return (printf("error: invalid color\n"), -1);
 	}
 	else if (!ft_strncmp("C ", tmp, 2) && ++(*nb_textures))
 	{
-		vars->ceilling_color = ft_substr(tmp, 2, ft_strlen(tmp + 2));
-		if (!vars->ceilling_color)
-			return (-1);
+		if (fill_colors(vars->ceilling_color, tmp + 1))
+			return (printf("error: invalid color\n"), -1);
 	}
+	else if (!ft_strncmp(tmp, "\n", 2))
+		return (0);
+	else
+		return (1);
 	return (0);
 }
 
@@ -128,11 +132,18 @@ int	fill_map(t_vars *vars, char *path, size_t size, int skip)
 		tmp = get_next_line(fd, &error);
 		if (error)
 			return (-1);
+		if (!ft_strncmp(tmp, "\n", 2))
+		{
+			free(tmp);
+			break ;	
+		}
 		vars->map[i] = ft_strdup(tmp);
 		if (!vars->map[i])
 			return (free(tmp), -1);
-		free(tmp);
+		if (vars->map[i][ft_strlen(tmp) - 1] == '\n')
+			vars->map[i][ft_strlen(tmp) - 1] = 0;
 		i++;
+		free(tmp);
 	}
 	return (vars->map[i] = NULL, close(fd), 0);
 }
