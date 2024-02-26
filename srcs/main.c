141d7a15d8515558c21deebf0d5365748f303970
Bilel <bilel.moudache@proton.me>
2024-02-26 18:08:18 +0100

    nothing change

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bmoudach <bmoudach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:09:51 by soutin            #+#    #+#             */
-/*   Updated: 2024/02/22 21:46:32 by bmoudach         ###   ########.fr       */
+/*   Updated: 2024/02/26 17:12:31 by bmoudach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,12 +129,15 @@ void	init_img(t_data *data)
 	int	i;
 
 	i = 0;
-	data->texture[0] = mlx_xpm_file_to_image(data->mlx_ptr, "./asset/map_asset/wall.xpm",
-			&(data->size_ximg), &(data->size_yimg));
-	data->texture[1] = mlx_xpm_file_to_image(data->mlx_ptr, "./asset/map_asset/player.xpm",
-			&(data->size_ximg), &(data->size_yimg));
-	data->texture[2] = mlx_xpm_file_to_image(data->mlx_ptr, "./asset/map_asset/floor.xpm",
-			&(data->size_ximg), &(data->size_yimg));
+	data->texture[0] = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./asset/map_asset/wall.xpm", &(data->size_ximg),
+			&(data->size_yimg));
+	data->texture[1] = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./asset/map_asset/player.xpm", &(data->size_ximg),
+			&(data->size_yimg));
+	data->texture[2] = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./asset/map_asset/floor.xpm", &(data->size_ximg),
+			&(data->size_yimg));
 	while (i < 3)
 	{
 		if (data->texture[i] == NULL)
@@ -150,8 +153,11 @@ void	display_map_2(t_data *data, int *i, int *j)
 	if (data->map[*j][*i] == '1')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->texture[0],
 			(*i * 50), (*j * 50));
-	if (data->map[*j][*i] == '0' || data->map[*j][*i] == 'S' ||data->map[*j][*i] == 'E'|| data->map[*j][*i] == 'W' ||data->map[*j][*i] == 'N')
+	if (data->map[*j][*i] == '0' || data->map[*j][*i] == 'N')
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->texture[2],
+			(*i * 50), (*j * 50));
+	if (data->map[*j][*i] == 'N')
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->texture[1],
 			(*i * 50), (*j * 50));
 }
 
@@ -173,6 +179,25 @@ void	display_map(t_data *data)
 		j++;
 	}
 }
+
+void	move(t_data *data, int x, int y)
+{
+	return ;
+}
+int	on_keypress(int keysym, t_data *data)
+{
+	if (keysym == 65307)
+		exit_and_free(data);
+	if (keysym == 119 || keysym == 65362)
+		move(data, 0, -1);
+	if (keysym == 115 || keysym == 65364)
+		move(data, 0, 1);
+	if (keysym == 97 || keysym == 65361)
+		move(data, -1, 0);
+	if (keysym == 100 || keysym == 65363)
+		move(data, 1, 0);
+	return (0);
+}
 int	main(int argc, char **argv)
 {
 	t_data	data;
@@ -189,6 +214,7 @@ int	main(int argc, char **argv)
 			ft_arraylen(data.map) * 50, "MazeCub3D");
 	if (!data.win_ptr)
 		return (free(data.win_ptr), -1);
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
 	init_img(&data);
 	display_map(&data);
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &exit_and_free,
