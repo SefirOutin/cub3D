b16629f95e52f5b7d600e8cab692dbed44ed800c
Bilel <bilel.moudache@proton.me>
2024-03-18 15:12:01 +0100

    norm func get 2.5d

--- srcs/graphic/oui.c
+++ srcs/graphic/oui.c
@@ -20,134 +20,134 @@ void	put_pixel_to_image(t_img *img, int x, int y, int color)
 	((img->addr))[pixel_index / (int)(img->bpp / 8)] = color;
 }
 
- int	view(t_data *data)
-{
-	int		x;
-	double	y_top;
-	double	y_bot;
-	double	wall_height;
-	int		half_height;
-	int		ratio;
-	int		i;
-
-	ratio = data->win.w / data->main_img.nb_rays;
-	half_height = data->win.h * 0.5;
-	data->main_img.view = init_img(data, data->win.w, data->win.h);
-	x = 0;
-	if (!data->main_img.view.img)
-		return (1);
-	while (x < data->win.w)
-	{
-		wall_height = data->win.h / data->main_img.rays_len[x / ratio] * 3;
-		y_bot = half_height;
-		y_top = half_height;
-		while (wall_height-- && y_top >= 0)
-		{
-			i = 0;
-			while (i < ratio)
-			{
-				put_pixel_to_image(&data->main_img.view, x + i, y_bot,
-					0x0000FF);
-				put_pixel_to_image(&data->main_img.view, x + i, y_top,
-					0x0000FF);
-				i++;
-			}
-			y_bot++;
-			y_top--;
-		}
-		while (y_bot < data->win.h)
-		{
-			i = 0;
-			while (i < ratio)
-				put_pixel_to_image(&data->main_img.view, x + i++, y_bot,
-					data->main_img.floor_color);
-			y_bot++;
-		}
-		while (y_top > -1)
-		{
-			i = 0;
-			while (i < ratio)
-				put_pixel_toぷmage(&data->main_img.view, x + i++, y_top,
-					data->main_img.ceilling_color);
-			y_top--;
-		}
-		x += ratio;
-	}
-	// mlx_destroy_image(data->mlx_ptr, data->main_img.view.img);
-	return (0);
-}
-
-//void	create_vertical_line(t_img *img, t_point start, int len, int color)
+// int	view(t_data *data)
 //{
-//	while (len--)
+//	int		x;
+//	double	y_top;
+//	double	y_bot;
+//	double	wall_height;
+//	int		half_height;
+//	int		ratio;
+//	int		i;
+//
+//	ratio = data->win.w / data->main_img.nb_rays;
+//	half_height = data->win.h * 0.5;
+//	data->main_img.view = init_img(data, data->win.w, data->win.h);
+//	x = 0;
+//	if (!data->main_img.view.img)
+//		return (1);
+//	while (x < data->win.w)
 //	{
-//		put_pixel_to_image(img, start.x, start.y, color);
-//		start.y++;
+//		wall_height = data->win.h / data->main_img.rays_len[x / ratio] * 3;
+//		y_bot = half_height;
+//		y_top = half_height;
+//		while (wall_height-- && y_top >= 0)
+//		{
+//			i = 0;
+//			while (i < ratio)
+//			{
+//				put_pixel_to_image(&data->main_img.view, x + i, y_bot,
+//					0x0000FF);
+//				put_pixel_to_image(&data->main_img.view, x + i, y_top,
+//					0x0000FF);
+//				i++;
+//			}
+//			y_bot++;
+//			y_top--;
+//		}
+//		while (y_bot < data->win.h)
+//		{
+//			i = 0;
+//			while (i < ratio)
+//				put_pixel_to_image(&data->main_img.view, x + i++, y_bot,
+//					data->main_img.floor_color);
+//			y_bot++;
+//		}
+//		while (y_top > -1)
+//		{
+//			i = 0;
+//			while (i < ratio)
+//				put_pixel_toぷmage(&data->main_img.view, x + i++, y_top,
+//					data->main_img.ceilling_color);
+//			y_top--;
+//		}
+//		x += ratio;
 //	}
-//}
-//
-//void	create_wall(t_data *data)
-//{
-//	int		color_wall;
-//	int		len_wall;
-//	t_point	start;
-//
-//	color_wall = 0x0000FF;
-//	start.x = 0;
-//	len_wall = 0;
-//	while (start.x < 640 && len_wall < 1080)
-//	{
-//		len_wall = data->win.h / ((data->main_img.rays_len[(int)start.x]));
-//		len_wall *= 15;
-//		start.y = (data->win.h / 2) - (len_wall / 2);
-//		create_vertical_line(&data->main_img.view, start, len_wall, color_wall);
-//		start.x++;
-//	}
-//}
-//void	create_floor(t_data *data)
-//{
-//	int		len_wall;
-//	int		len_floor;
-//	t_point	start;
-//
-//	start.x = 0;
-//	while (start.x < 640)
-//	{
-//		len_wall = data->win.h / data->main_img.rays_len[(int)start.x];
-//		len_wall *= 15;
-//		start.y = (data->win.h / 2) + (len_wall / 2);
-//		len_floor = data->win.h - start.x;
-//		create_vertical_line(&data->main_img.view, start, len_floor,
-//			data->main_img.floor_color);
-//		start.x++;
-//	}
-//}
-//
-//void	create_sky(t_data *data)
-//{
-//	int		len_wall;
-//	int		len_sky;
-//	t_point	start;
-//
-//	start.x = 0;
-//	while (start.x < 640)
-//	{
-//		len_wall = data->win.h / data->main_img.rays_len[(int)start.x];
-//		start.y = 0;
-//		len_wall *= 15;
-//		len_sky = (data->win.h / 2) - (len_wall / 2);
-//		create_vertical_line(&data->main_img.view, start, len_sky,
-//			data->main_img.ceilling_color);
-//		start.x++;
-//	}
-//}
-//
-//int	view(t_data *data)
-//{
-//	if (init_img(data, &data->main_img.view, data->win.w, data->win.h))
-//		return (-1);
-//	create_wall(data);
-//	// create_floor(data);
-//	create_sky(data);
+//	// mlx_destroy_image(data->mlx_ptr, data->main_img.view.img);
 //	return (0);
 //}
+
+void	create_vertical_line(t_img *img, t_point start, int len, int color)
+{
+	while (len--)
+	{
+		put_pixel_to_image(img, start.x, start.y, color);
+		start.y++;
+	}
+}
+
+void	create_wall(t_data *data)
+{
+	int		color_wall;
+	int		len_wall;
+	t_point	start;
+
+	color_wall = 0x0000FF;
+	start.x = 0;
+	len_wall = 0;
+	while (start.x < 640 && len_wall < 1080)
+	{
+		len_wall = data->win.h / ((data->main_img.rays_len[(int)start.x]));
+		len_wall *= 15;
+		start.y = (data->win.h / 2) - (len_wall / 2);
+		create_vertical_line(&data->main_img.view, start, len_wall, color_wall);
+		start.x++;
+	}
+}
+void	create_floor(t_data *data)
+{
+	int		len_wall;
+	int		len_floor;
+	t_point	start;
+
+	start.x = 0;
+	while (start.x < 640)
+	{
+		len_wall = data->win.h / data->main_img.rays_len[(int)start.x];
+		len_wall *= 15;
+		start.y = (data->win.h / 2) + (len_wall / 2);
+		len_floor = (data->win.h / 2) - (len_wall / 2);
+		create_vertical_line(&data->main_img.view, start, len_floor,
+			data->main_img.floor_color);
+		start.x++;
+	}
+}
+
+void	create_sky(t_data *data)
+{
+	int		len_wall;
+	int		len_sky;
+	t_point	start;
+
+	start.x = 0;
+	while (start.x < 640)
+	{
+		len_wall = data->win.h / data->main_img.rays_len[(int)start.x];
+		start.y = 0;
+		len_wall *= 15;
+		len_sky = (data->win.h / 2) - (len_wall / 2);
+		create_vertical_line(&data->main_img.view, start, len_sky,
+			data->main_img.ceilling_color);
+		start.x++;
+	}
+}
+
+int	view(t_data *data)
+{
+	if (init_img(data, &data->main_img.view, data->win.w, data->win.h))
+		return (-1);
+	create_wall(data);
+	create_floor(data);
+	create_sky(data);
+	return (0);
+}
--- srcs/graphic/ray.c
+++ srcs/graphic/ray.c
@@ -6,7 +6,7 @@
 /*   By: bmoudach <bmoudach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:48:27 by soutin            #+#    #+#             */
-/*   Updated: 2024/03/16 15:01:30 by bmoudach         ###   ########.fr       */
+/*   Updated: 2024/03/18 15:04:32 by bmoudach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,25 @@ void	find_point_on_section(t_ray *ray, double len)
 	ray->end.y = round(ray->end.y - len * ray->len_one_u.y);
 }
 
-int diff_nearest_50x(int x)
+int	diff_nearest_50x(int x)
 {
-    // Calcul du multiple de 50 le plus proche
-    int nearestMultiple = 50 * (x / 50);
-    // Calcul de la différence
-    int difference = x - nearestMultiple;
-	
+	int	nearestMultiple;
+	int	difference;
+
+	// Calcul du multiple de 50 le plus proche
+	nearestMultiple = 50 * (x / 50);
+	// Calcul de la différence
+	difference = x - nearestMultiple;
 	if (difference > 25)
 		return (50 - difference);
-    return (difference);
+	return (difference);
 }
-
 
 int	get_quadrant(int angle)
 {
 	if (angle >= 0 && angle < 89)
 		return (0);
-	if (angle >=90  && angle < 180)
+	if (angle >= 90 && angle < 180)
 		return (1);
 	if (angle >= 180 && angle < 270)
 		return (2);
@@ -63,17 +64,21 @@ int	get_quadrant(int angle)
 int	check_next_edges(t_data *data, t_ray *ray)
 {
 	t_point	delta;
-	
+
 	// calcule la distance du joueur par rapport aux bords de la case que le joueur regarde (sert à vlen)
 	if (ray->angle_deg > 0 && ray->angle_deg < 180)
-		delta.y = ray->end.y - (floor(ray->end.y  * 0.02) * data->minimap.size) + 1;
+		delta.y = ray->end.y - (floor(ray->end.y * 0.02) * data->minimap.size)
+			+ 1;
 	else
-		delta.y = (ceil(ray->end.y * 0.02) * data->minimap.size) - ray->end.y + 1;
+		delta.y = (ceil(ray->end.y * 0.02) * data->minimap.size) - ray->end.y
+			+ 1;
 	if ((ray->angle_deg > 90 && ray->angle_deg < 270))
-		delta.x = ray->end.x - (floor(ray->end.x * 0.02) * data->minimap.size) + 1;
+		delta.x = ray->end.x - (floor(ray->end.x * 0.02) * data->minimap.size)
+			+ 1;
 	else
-		delta.x = (ceil(ray->end.x * 0.02) * data->minimap.size) - ray->end.x + 1;
-	// calcule du vecteur pour prochain x entier et prochain y entier pour ensuite les comparer 
+		delta.x = (ceil(ray->end.x * 0.02) * data->minimap.size) - ray->end.x
+			+ 1;
+	// calcule du vecteur pour prochain x entier et prochain y entier pour ensuite les comparer
 	ray->vlen.x = delta.x * ray->hypo_len_one_u.x;
 	ray->vlen.y = delta.y * ray->hypo_len_one_u.y;
 	// printf("vlen x:%f Y:%f\n", ray->vlen.x, ray->vlen.y);
@@ -87,25 +92,22 @@ int	check_angles(t_data *data, t_ray *ray)
 {
 	t_point	first;
 	t_point	second;
-	
+
 	first.x = (int)(ray->end.x * 0.02);
 	first.y = (int)(ray->end.y * 0.02);
 	second.x = (int)(ray->end.x * 0.02);
 	second.y = (int)(ray->end.y * 0.02);
-	if((abs(diff_nearest_50x(ray->end.x) * diff_nearest_50x(ray->end.y)) < 2))
-	{
-		if (ray->angle_deg > 180)
-			first.y--;
-		else
-			first.y++;
-		if(ray->end.x < data->player.pos.x)
-			second.x++;
-		else if(ray->end.x > data->player.pos.x)
-			second.x--;	
-		if (data->map[(int)first.y][(int)first.x] == '1'
+	if (ray->angle_deg > 180)
+		first.y--;
+	else
+		first.y++;
+	if (ray->end.x < data->player.pos.x)
+		second.x++;
+	else if (ray->end.x > data->player.pos.x)
+		second.x--;
+	if (data->map[(int)first.y][(int)first.x] == '1'
 		&& data->map[(int)second.y][(int)second.x] == '1')
-			return (1);
-	}
+		return (1);
 	return (0);
 }
 
@@ -114,16 +116,18 @@ void	find_next_wall(t_data *data, t_ray *ray, double curr_ray)
 	// t_point	len;
 	// int i = 0;
 	// int ratio;
-	
 	ray->angle_deg = fix_ang(data->player.direction + curr_ray - FOV * 0.5);
 	ray->angle_rad = deg_to_rad(ray->angle_deg);
 	ray->len_one_u.x = cos(ray->angle_rad);
 	ray->len_one_u.y = sin(ray->angle_rad);
 	ray->end.x = data->player.pos.x;
 	ray->end.y = data->player.pos.y;
-	ray->hypo_len_one_u.x =  sqrt(1 + pow((ray->len_one_u.y) / (ray->len_one_u.x), 2));
-	ray->hypo_len_one_u.y = sqrt(1 + pow((ray->len_one_u.x) / (ray->len_one_u.y), 2));
-	while ((data->map[(int)(ray->end.y * 0.02)][(int)(ray->end.x * 0.02)] != '1'))
+	ray->hypo_len_one_u.x = sqrt(1 + pow((ray->len_one_u.y)
+				/ (ray->len_one_u.x), 2));
+	ray->hypo_len_one_u.y = sqrt(1 + pow((ray->len_one_u.x)
+				/ (ray->len_one_u.y), 2));
+	while ((data->map[(int)(ray->end.y * 0.02)][(int)(ray->end.x
+				* 0.02)] != '1'))
 	{
 		check_next_edges(data, ray);
 		if (check_angles(data, ray))
@@ -137,18 +141,17 @@ void	display_rays(t_data *data, t_ray *ray)
 	int		i;
 	double	ratio;
 	int		num_points;
-	
+
 	i = 0;
 	num_points = ray->len * 0.2;
-	while (i  < num_points)
+	while (i < num_points)
 	{
 		ratio = (double)i / (double)(num_points - 1);
-		mlx_pixel_put(data->win.mlx_ptr, data->win.win_ptr,
-			data->player.pos.x + ratio * (ray->end.x - data->player.pos.x),
-			data->player.pos.y +  ratio * (ray->end.y - data->player.pos.y), 0x7FFF00);
+		mlx_pixel_put(data->win.mlx_ptr, data->win.win_ptr, data->player.pos.x
+			+ ratio * (ray->end.x - data->player.pos.x), data->player.pos.y
+			+ ratio * (ray->end.y - data->player.pos.y), 0x7FFF00);
 		i++;
 	}
-	
 }
 
 int	create_rays(t_data *data)
@@ -156,7 +159,7 @@ int	create_rays(t_data *data)
 	t_ray	ray;
 	int		curr_ray;
 	double	angle_ratio;
-	
+
 	curr_ray = 0;
 	angle_ratio = 0.140625;
 	while (curr_ray < data->main_img.nb_rays)
